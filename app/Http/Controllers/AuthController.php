8e9a88364553309fa00132d09e5c312f3c5b8a21
shundahoy <shundahoy@gmail.com>
2022-12-05 22:11:54 +0900

    first commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -17,8 +19,36 @@ class AuthController extends Controller
             'first_name' => $request->input('first_name'),
             'last_name' => $request->input('last_name'),
             'email' => $request->input('email'),
-            'password' =>  Hash::make($request->input('email')),
+            'password' =>  Hash::make($request->input('password')),
         ]);
         return response($user, Response::HTTP_CREATED);
+    }
+
+    public function login(Request $request)
+    {
+
+        $credentials = $request->only('email', 'password');
+        if (!Auth::attempt($credentials)) {
+            return \response(['error' => 'invalid credentials', 'request' => $request->only('email', 'password'), Auth::attempt($credentials)], Response::HTTP_UNAUTHORIZED);
+        }
+
+        /**  @var User $user */
+        $user = Auth::user();
+        $jwt = $user->createToken('token')->plainTextToken;
+
+        $cookie = cookie('jwt', $jwt, 60 * 24);
+        return \response(['jwt' => $jwt])->withCookie($cookie);
+    }
+    public function user(Request $request)
+    {
+        return $request->user();
+    }
+
+    public function logout(Request $request)
+    {
+        $cookie = Cookie::forget('jwt');
+        // $request->user()->currentAccessToken()->delete();
+        $request->user()->tokens()->delete();
+        return response(['message' => 'success'])->withCookie(($cookie));
     }
 }
